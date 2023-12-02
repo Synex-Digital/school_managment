ae6027020e3eb5e546c52842329d25ecd8dfc9f6
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-12-03 01:34:09 +0600

    aside part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#0C0C0C",
-        Secondary: "#FB2576",
-        navcolor: "#1C0033",
-      },
-      spacing: {
-        container: "1320px",
-      },
-      fontFamily: {
-        in: "'Inter', sans-serif",
-      },
-      spacing: {
-        "30px": "30px",
-        "60px": "60px",
-      },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#0C0C0C",
+                Secondary: "#FB2576",
+                navcolor: "#1C0033",
+                bordercolor: "#C2C2C2",
+                ptagcolor: "#303030",
+            },
+            spacing: {
+                container: "1320px",
+            },
+            fontFamily: {
+                in: "'Inter', sans-serif",
+            },
+            spacing: {
+                "30px": "30px",
+                "32px": "32px",
+                "60px": "60px",
+            },
+            fontSize: {
+              hd: '32px',
+            }
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
